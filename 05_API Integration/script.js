2af7069882858e06e14c6f4f4c030b5f48af0f65
Kajalti Sirame <kajalti@29>
2026-02-23 22:37:27 +0530

    cose commit

--- 05_API Integration/script.js
+++ 05_API Integration/script.js
@@ -1,32 +1,29 @@
 const button = document.getElementById("loadBtn");
 const container = document.getElementById("userContainer");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", async function () {
 
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
 
-      container.innerHTML = ""; // Clear previous data
+    container.innerHTML = "";
 
-      data.forEach(function(user) {
-        const div = document.createElement("div");
-        div.classList.add("user-card");
+    data.forEach(user => {
+      const div = document.createElement("div");
+      div.classList.add("user-card");
 
-        div.innerHTML = `
-          <h3>${user.name}</h3>
-          <p>Email: ${user.email}</p>
-          <p>City: ${user.address.city}</p>
-        `;
+      div.innerHTML = `
+        <h3>${user.name}</h3>
+        <p>Email: ${user.email}</p>
+        <p>City: ${user.address.city}</p>
+      `;
 
-        container.appendChild(div);
-      });
-
-    })
-    .catch(function(error) {
-      console.log("Error:", error);
+      container.appendChild(div);
     });
+
+  } catch (error) {
+    console.log("Error:", error);
+  }
 
 });
